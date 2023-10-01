0b7ac30684a5d2d7fe8cdf18ad835d6ddb8ec892
Juan Aguilar <juanpabloaguilar@outlook.com>
2023-09-30 19:19:53 -0500

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav className="navprincipal">
+      <div>Inicio</div>
+      <div>El Sistema Internacional</div>
+    </nav>
+  );
+}
+
+export default Nav;
